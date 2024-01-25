618942b68cec7ba80ccf9da2feecce2a22ff1b71
oshuakbaev <olzhason47@gmail.com>
2024-01-25 10:15:24 +0600

    update

---
+++ Hotel.ATR.Portal2/Hotel.ATR.Portal2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hotel.ATR.Portal2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
